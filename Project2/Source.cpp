017bcaffc92198703f0f4406ee2c7ec6973f1cd7
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2018-09-12 01:23:50 +0900

    畳表示

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -15,7 +15,7 @@ int WINDOW_W = 800;    //生成するウィンドウの幅
 int WINDOW_H = 800;    //生成するウィンドウの高さ
 char WindowTitle[] = "世界の始まり";  //ウィンドウのタイトル
 color_image4_t texture;
-GLuint texture_id[8];
+GLuint texture_id[9];
 const char * texture_path[] =
 {
 	"img/kyousha.data",
@@ -25,7 +25,8 @@ const char * texture_path[] =
 	"img/ou.data",
 	"img/hu.data",
 	"img/kaku.data",
-	"img/hisha.data"
+	"img/hisha.data",
+	"img/tatami.data"
 };
 //----------------------------------------------------
 // 関数プロトタイプ（後に呼び出す関数名と引数の宣言）
@@ -33,25 +34,16 @@ const char * texture_path[] =
 void Initialize(void);
 void Display(void);
 void Ground(void);  //大地の描画
-void initTexMode(GLenum tex_mode, GLint filter_mode, GLint wrap_mode, GLint env_mode)
-{
-	glTexParameteri(tex_mode, GL_TEXTURE_MAG_FILTER, filter_mode);
-	glTexParameteri(tex_mode, GL_TEXTURE_MIN_FILTER, filter_mode);
-	glTexParameteri(tex_mode, GL_TEXTURE_WRAP_S, wrap_mode);
-	glTexParameteri(tex_mode, GL_TEXTURE_WRAP_T, wrap_mode);
-	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, env_mode);
-}
 inline double scaler(int point, double base) {
 	return (point - base);
 }
-
 void draw_koma() {
 	glPushMatrix();
 
 	glTranslated(0, 0, 10);
 	glTranslated(9, 9, 0);
 	glScaled(0.05, 0.05, 0.05);
-	glColor3d(253/255.0 , 251/255.0 ,70/255.0);
+	glColor3d(253 / 255.0, 251 / 255.0, 70 / 255.0);
 	glRotated(180, 0, 0, 1);
 
 	//下面
@@ -140,22 +132,22 @@ void Initialize(void)
 	glEnable(GL_DEPTH_TEST);
 	// テクスチャの読み込み
 	glGenTextures(8, texture_id);
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < sizeof(texture_path) / sizeof(texture_path[0]); i++)
 	{
 		load_raw_image(&texture, texture_path[i]);
 		glBindTexture(GL_TEXTURE_2D, texture_id[i]);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.width, texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture.image);
 
 		// テクスチャの各種設定
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.width, texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture.image);
 	}
 	/* テクスチャを拡大・縮小する方法の指定 */
 	glAlphaFunc(GL_GREATER, 0.7);
 	gluPerspective(30.0, (double)WINDOW_W / (double)WINDOW_H, 0.1, 1000.0); //透視投影法の視体積gluPerspactive(th, w/h, near, far);
-	//initTexMode(GL_TEXTURE_2D, GL_LINEAR, GL_CLAMP, GL_MODULATE);
 	gluLookAt(
 		0.0, -200.0, 300.0, // 視点の位置x,y,z;
 		0.0, 0.0, 0.0,   // 視界の中心位置の参照点座標x,y,z
@@ -231,17 +223,38 @@ void Display(void)
 //----------------------------------------------------
 void Ground(void)
 {
-	double ground_max_x = 300.0;
-	double ground_max_y = 300.0;
-	glColor3d(0.8, 0.8, 0.8);  // 大地の色
-	glBegin(GL_LINES);
-	for (double ly = -ground_max_y; ly <= ground_max_y; ly += 10.0) {
-		glVertex3d(-ground_max_x, ly, 0);
-		glVertex3d(ground_max_x, ly, 0);
-	}
-	for (double lx = -ground_max_x; lx <= ground_max_x; lx += 10.0) {
-		glVertex3d(lx, ground_max_y, 0);
-		glVertex3d(lx, -ground_max_y, 0);
-	}
+	//double ground_max_x = 300.0;
+	//double ground_max_y = 300.0;
+	//glColor3d(0.8, 0.8, 0.8);  // 大地の色
+	//glBegin(GL_LINES);
+	//for (double ly = -ground_max_y; ly <= ground_max_y; ly += 10.0) {
+	//	glVertex3d(-ground_max_x, ly, 0);
+	//	glVertex3d(ground_max_x, ly, 0);
+	//}
+	//for (double lx = -ground_max_x; lx <= ground_max_x; lx += 10.0) {
+	//	glVertex3d(lx, ground_max_y, 0);
+	//	glVertex3d(lx, -ground_max_y, 0);
+	//}
+	//glEnd();
+	/* テクスチャの繰り返しの指定 */
+	glPushMatrix();
+	glScaled(0.1, 0.1, 1);
+	glBindTexture(GL_TEXTURE_2D, texture_id[8]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glEnable(GL_TEXTURE_2D);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, white);
+	glBegin(GL_QUADS);
+	glTexCoord2d(0, 0);
+	glVertex3d(-3000, 3000, -10);
+	glTexCoord2d(0, 100);
+	glVertex3d(-3000, -30000, -10);
+	glTexCoord2d(100, 100);
+	glVertex3d(3000, -3000, -10);
+	glTexCoord2d(100, 0);
+	glVertex3d(3000, 3000, -10);
 	glEnd();
+	glDisable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D,0);
+	glPopMatrix();
 }
