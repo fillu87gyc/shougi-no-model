42616fe43d584f07c2a01ba6993141c574e59808
fillu87gyc <s1500604@sendai.kosen-ac.jp>
2018-09-12 03:11:14 +0900

    音付きで完成

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -8,7 +8,15 @@
 #include "color.h"
 #include "material.h"
 #include "image.h"
-
+#pragma comment(lib,"Winmm.lib")//この行の代わりに「追加の依存ファイル」に追加しても良い
+#include <mmsystem.h>
+//使用音源
+//http://shw.in/sozai/japan.php
+//よみ2
+//よみ2(尺八オフ)
+//4：53	slow	和風汎用BGM　／　琴・尺八・三味線
+//和風・ダーク・暗い
+//0：49程度のフレーズを繰り返します	2012年4月
 int WindowPositionX = 100;  //生成するウィンドウ位置のX座標
 int WindowPositionY = 100;  //生成するウィンドウ位置のY座標
 int WINDOW_W = 800;    //生成するウィンドウの幅
@@ -62,11 +70,24 @@ void draw_koma() {
 	glVertex3d(0, 385, 30);
 	glVertex3d(40, 40, 30);
 	glEnd();
+
+	//線
+	glColor3d(0, 0, 0);
+	glLineWidth(3);
+	glBegin(GL_LINE_LOOP);
+	glVertex3d(180, 0, 30);
+	glVertex3d(300, 40, 30);
+	glVertex3d(360, 385, 30);
+	glVertex3d(0, 385, 30);
+	glVertex3d(40, 40, 30);
+	glEnd();
+	glLineWidth(1);
+	glColor3d(253 / 255.0, 251 / 255.0, 70 / 255.0);
 	//テクスチャ
-	glTranslated(0, 0, 1);
+	glTranslated(0, 0, 2);
 	glEnable(GL_ALPHA_TEST);
 	glEnable(GL_TEXTURE_2D);
-	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, white);
+	//glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, white);
 	glBegin(GL_QUADS);
 	glTexCoord2d(0, 0);
 	glVertex3d(360, 40, 30);
@@ -83,13 +104,14 @@ void draw_koma() {
 }
 void drawboard() {
 	glPushMatrix();
-	glColor3d(0.0, 1.0, 0.0);//色の設定
+	glColor3d(1.0, 1.0, 0.0);//色の設定
 	glLineWidth(3);
-	glTranslated(0, 0, 5);
-	glScaled(18, 18, 1);
+	glScaled(18, 18, 3.3);
+	glTranslated(0, 0, -1.65);
 	glutSolidCube(10.0);//引数：(一辺の長さ)
+	glColor3d(0, 0, 0);
+	glutWireCube(10.001);
 	glColor3d(0, 0, 0);//色の設定
-	glutWireCube(10.01);
 	double ground_max_x = 5.0;
 	double ground_max_y = 5.0;
 	glPushMatrix();
@@ -127,6 +149,7 @@ int main(int argc, char *argv[]) {
 //----------------------------------------------------
 void Initialize(void)
 {
+	PlaySound("sound/yomi2_mixv2.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	// 隠面処理の有効
 	glEnable(GL_DEPTH_TEST);
@@ -148,8 +171,10 @@ void Initialize(void)
 	/* テクスチャを拡大・縮小する方法の指定 */
 	glAlphaFunc(GL_GREATER, 0.7);
 	gluPerspective(30.0, (double)WINDOW_W / (double)WINDOW_H, 0.1, 1000.0); //透視投影法の視体積gluPerspactive(th, w/h, near, far);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
 	gluLookAt(
-		0.0, -200.0, 300.0, // 視点の位置x,y,z;
+		0.0, -230.0, 330.0, // 視点の位置x,y,z;
 		0.0, 0.0, 0.0,   // 視界の中心位置の参照点座標x,y,z
 		0.0, 0.0, 1.0);  //視界の上方向のベクトルx,y,z
 }
@@ -209,6 +234,7 @@ void Display(void)
 	glTranslated(-20 * 5, -20 * 4, 0);
 	set_koma();
 	glPopMatrix();
+
 	glPushMatrix();
 	glRotated(180, 0, 0, 1);
 	//左下に調整
@@ -238,12 +264,13 @@ void Ground(void)
 	//glEnd();
 	/* テクスチャの繰り返しの指定 */
 	glPushMatrix();
+	glTranslated(0, 0, -100);
 	glScaled(0.1, 0.1, 1);
 	glBindTexture(GL_TEXTURE_2D, texture_id[8]);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glEnable(GL_TEXTURE_2D);
-	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, white);
+	//glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, white);
 	glBegin(GL_QUADS);
 	glTexCoord2d(0, 0);
 	glVertex3d(-3000, 3000, -10);
@@ -255,6 +282,6 @@ void Ground(void)
 	glVertex3d(3000, 3000, -10);
 	glEnd();
 	glDisable(GL_TEXTURE_2D);
-	glBindTexture(GL_TEXTURE_2D,0);
+	glBindTexture(GL_TEXTURE_2D, 0);
 	glPopMatrix();
 }
